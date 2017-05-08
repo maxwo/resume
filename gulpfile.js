7a66d9b574c76f0a406fd7561b3ad49136b228b2
Maxime Wojtczak <maxime.wojtczak@zenika.com>
2017-05-08 16:10:06 +0200

    style(gulpfile) : Enhance code style.

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,68 @@
-var gulp = require('gulp'),
-    util = require('gulp-util'),
-    jade = require('gulp-jade'),
-    sass = require('gulp-sass'),
-    connect = require('gulp-connect'),
-    html2pdf = require('gulp-html2pdf'),
-    _ = require('underscore'),
-    argv = require('yargs').argv;
+const gulp = require('gulp');
+const util = require('gulp-util');
+const jade = require('gulp-jade');
+const sass = require('gulp-sass');
+const connect = require('gulp-connect');
+const html2pdf = require('gulp-html2pdf');
+const argv = require('yargs').argv;
 
-gulp.task('pdf', function() {
-    return gulp.src('./*.html').pipe(html2pdf({printMediaType: true, pageSize: "letter"})).pipe(gulp.dest('./pdf/'));
-});
+gulp.task('pdf',
+  () => gulp
+    .src('./*.html')
+    .pipe(html2pdf({
+      printMediaType: true,
+      pageSize: 'letter',
+    }))
+    .pipe(gulp.dest('./pdf/')));
 
-gulp.task('styles', function() {
-    return gulp.src('./scss/**/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./dist/styles/')).pipe(connect.reload());
-});
+gulp.task('styles', () =>
+  gulp
+    .src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/styles/'))
+    .pipe(connect.reload()));
 
-gulp.task('templates', function() {
-    return gulp.src('./templates/**/*.jade').pipe(jade({
-        locals: {
-            company_name: argv.company_name,
-            position: argv.position,
-            name: argv.name
-        },
-        pretty: true
-    }).on('error', util.log)).pipe(gulp.dest('./dist/')).pipe(connect.reload());
-});
+gulp.task('templates', () =>
+  gulp
+    .src('./templates/**/*.jade')
+    .pipe(jade({
+      locals: {
+        company_name: argv.company_name,
+        position: argv.position,
+        name: argv.name,
+      },
+      pretty: true,
+    })
+    .on('error', util.log))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload()));
 
-gulp.task('images', function() {
-  return gulp.src(['images/**/*']).pipe(gulp.dest('dist/images'));
-});
+gulp.task('images', () =>
+  gulp
+    .src(['images/**/*'])
+    .pipe(gulp.dest('dist/images')));
 
-gulp.task('watchStyles', function() {
-    gulp.watch('./scss/**/*.scss', ['styles']);
-});
+gulp.task('watchStyles', () =>
+  gulp
+    .watch('./scss/**/*.scss', ['styles']));
 
-gulp.task('watchTemplates', function() {
-    gulp.watch('./templates/**/*.jade', ['templates']);
-});
+gulp.task('watchTemplates', () =>
+  gulp.watch('./templates/**/*.jade', ['templates']));
 
-gulp.task('connect', function() {
-    connect.server({root: './dist/', port: 8081, livereload: true});
-});
+gulp.task('connect', () =>
+  connect.server({
+    root: './dist/',
+    port: 8081,
+    livereload: true,
+  }));
 
 gulp.task('build', ['styles', 'templates', 'images']);
 
 gulp.task('default', [
-    'styles',
-    'templates',
-    'images',
-    'watchStyles',
-    'watchTemplates',
-    'connect'
+  'styles',
+  'templates',
+  'images',
+  'watchStyles',
+  'watchTemplates',
+  'connect',
 ]);
