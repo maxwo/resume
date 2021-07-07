7ec3775a3f79eb255d71c63ae8c74a4a9b364b34
Maxime Wojtczak <maxime.wojtczak@zenika.com>
2021-07-07 11:23:26 +0200

    update certifications

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const connect = require('gulp-connect');
 
 gulp.task('pdf', function() {
